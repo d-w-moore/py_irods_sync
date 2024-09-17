e757c07759c6ee03ae55fa3ef94e6df6df1a181e
Daniel Moore <dmoore@renci.org>
2024-09-17 05:57:10 -0400

    Update dll.c
    add more DLL function

--- dll.c
+++ dll.c
@@ -3,14 +3,16 @@ void f(unsigned char * c) {
 }
 
 #include <stdint.h>
-
-#a and b pointers should refer to buffers of at least nrep + 1 elements
+#define ADLER_PRIME 65521
+# checksum, a, and b pointers should refer to buffers of at least nrep + 1 elements
 void gen_weak_checksums(uint8_t *large_win, int offs, int nrep, int *checksums, int *a, int *b, int blksize)
 {
     uint8_t *new = &large_win[blksize];
     uint8_t *removed = &large_win[0];
     for (int i = 1; i <= nrep; i++,new++,removed++) {
         *a += (*new) - (*removed);
-        *b++ += *a++ - (*removed) * blksize;
+        *b += *a - (*removed) * blksize;
+        * checksum ++ = ((*b++) % ADLER_PRIME)<<16 | 
     }
+    
 }
