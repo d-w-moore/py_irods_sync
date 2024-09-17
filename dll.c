646513ecfcedf1a83b15e38edc4be601170ec621
d-w-moore <dmoore@renci.org>
2024-09-17 05:41:57 -0400

    first real commit

---
+++ dll.c
@@ -0,0 +1,16 @@
+void f(unsigned char * c) {
+  c[0] += 1;
+}
+
+#include <stdint.h>
+
+#a and b pointers should refer to buffers of at least nrep + 1 elements
+void gen_weak_checksums(uint8_t *large_win, int offs, int nrep, int *checksums, int *a, int *b, int blksize)
+{
+    uint8_t *new = &large_win[blksize];
+    uint8_t *removed = &large_win[0];
+    for (int i = 1; i <= nrep; i++,new++,removed++) {
+        *a += (*new) - (*removed);
+        *b++ += *a++ - (*removed) * blksize;
+    }
+}
